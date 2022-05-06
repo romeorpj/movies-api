39dbb5a03db541b279aa5eb7ac3e567a68af871b
Richard Jackson <rpjackson2@gmail.com>
2022-05-06 12:30:11 -0500

    setup many to many tables

--- src/main/resources/movies-create-script.sql
+++ src/main/resources/movies-create-script.sql
@@ -1,51 +1,85 @@
-# 1. go get the json file from glitch
-# 2. copypasta into a new json file under /resources
 
-# --> You may need to establish a connection to your localhost db here
+
+# 1. go get the json file from glitch
+
+# 2. copy paste into a new json file under /resources
 
 # 3. create the movies_db
 
-# 4. use the movies_db
-
-# 5. drop the table(s) to which no other tables are dependent (none at first)
-
-# 6. map the json movie properties to movies table columns
+# 4. map the json movie properties to movies table columns
 # --> start with just a movies table with all the columns found in the movie json properties
 
-# 6a. Run the script to make sure it works
+create database if not exists movies_db;
 
-# 7. refactor to extract the directors to a new table with just an id and name
-# --> change the movies table to reference the directors table via Foreign Key
-# --> now that movies is dependent on directors, you need to move directors above movies in the script
+use movies_db;
 
-# 8. Go add DROP IF EXIST statements for movies and directors
+drop table if exists movie_genre;
+drop table if exists movie_actor;
+drop table if exists actors;
+drop table if exists genres;
+drop table if exists movies;
+drop table if exists directors;
 
-# 9. RUN IT!
-DROP DATABASE IF EXISTS movies_db;
-CREATE DATABASE IF NOT EXISTS  movies_db;
-USE movies_db;
 
-DROP TABLE IF EXISTS movies;
-DROP TABLE IF EXISTS directors;
-
-CREATE TABLE directors (
-id INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY ,
-name VARCHAR(255)
+create table if not exists directors
+(
+    id int unsigned not null auto_increment primary key,
+    name varchar(120)
 );
 
+DESC directors;
+
+
 CREATE TABLE IF NOT EXISTS movies (
-    id INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    title VARCHAR(120) NOT NULL,
-    director_id INT UNSIGNED NOT NULL,
-    actors VARCHAR(255),
-    plot TEXT,
-    rating CHAR(1),
-    year VARCHAR(4) NOT NULL,
-    genre VARCHAR(120),
-    poster VARCHAR(255),
+id INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+title VARCHAR(120) NOT NULL,
+director_id INT UNSIGNED NOT NULL,
+actors VARCHAR(255),
+plot TEXT,
+rating CHAR(1),
+year VARCHAR(4) NOT NULL,
+genre VARCHAR(120),
+poster VARCHAR(255),
 FOREIGN KEY (director_id) REFERENCES directors(id)
 );
 
-DESC movies;
+describe movies;
 
 
+create table if not exists genres
+(
+    id   int unsigned not null auto_increment primary key,
+    name varchar(120)
+);
+
+DESC genres;
+
+
+create table if not exists movie_genre
+(
+    movie_id int unsigned not null,
+    genre_id int unsigned not null,
+    foreign key (movie_id) references movies(id),
+    foreign key (genre_id) references genres(id)
+
+);
+
+DESC movie_genre;
+
+create table if not exists actors
+(
+    id int unsigned not null auto_increment primary key,
+    name varchar(255)
+);
+
+DESCactors;
+
+create table if not exists movie_actor
+(
+    movie_id int unsigned not null,
+    actor_id int unsigned not null,
+    foreign key (movie_id) references movies (id),
+    foreign key (actor_id) references actors (id)
+);
+
+DESC movie_actor;
