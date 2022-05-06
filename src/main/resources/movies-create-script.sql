93d15aaa73857096ba4ac9c45d3174d378762b39
Richard Jackson <rpjackson2@gmail.com>
2022-05-06 12:08:48 -0500

    Creating movies backend database

--- src/main/resources/movies-create-script.sql
+++ src/main/resources/movies-create-script.sql
@@ -21,7 +21,31 @@
 # 8. Go add DROP IF EXIST statements for movies and directors
 
 # 9. RUN IT!
+DROP DATABASE IF EXISTS movies_db;
+CREATE DATABASE IF NOT EXISTS  movies_db;
+USE movies_db;
 
+DROP TABLE IF EXISTS movies;
+DROP TABLE IF EXISTS directors;
 
+CREATE TABLE directors (
+id INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY ,
+name VARCHAR(255)
+);
+
+CREATE TABLE IF NOT EXISTS movies (
+    id INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(120) NOT NULL,
+    director_id INT UNSIGNED NOT NULL,
+    actors VARCHAR(255),
+    plot TEXT,
+    rating CHAR(1),
+    year VARCHAR(4) NOT NULL,
+    genre VARCHAR(120),
+    poster VARCHAR(255),
+FOREIGN KEY (director_id) REFERENCES directors(id)
+);
+
+DESC movies;
 
 
