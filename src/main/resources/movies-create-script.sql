14f7f89b70788cf4fe044dc9cc3822fd0a245704
Richard Jackson <rpjackson2@gmail.com>
2022-05-12 13:39:49 -0500

    adding directors table

--- src/main/resources/movies-create-script.sql
+++ src/main/resources/movies-create-script.sql
@@ -1,59 +1,57 @@
-
-
 # 1. go get the json file from glitch
+# 2. copypasta into a new json file under /resources
 
-# 2. copy paste into a new json file under /resources
+# --> You may need to establish a connection to your localhost db here
 
 # 3. create the movies_db
-
-# 4. map the json movie properties to movies table columns
-# --> start with just a movies table with all the columns found in the movie json properties
-
 create database if not exists movies_db;
 
+# 4. use the movies_db
 use movies_db;
 
-drop table if exists movie_genre;
-drop table if exists movie_actor;
-drop table if exists actors;
-drop table if exists genres;
+# 5. drop the table(s) to which no other tables are dependent (none at first)
 drop table if exists movies;
 drop table if exists directors;
 
-
+# 6. map the json movie properties to movies table columns
+# --> start with just a movies table with all the columns found in the movie json properties
 create table if not exists directors
 (
     id int unsigned not null auto_increment primary key,
     name varchar(120)
 );
 
-DESC directors;
-
-
-CREATE TABLE IF NOT EXISTS movies (
-id INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
-title VARCHAR(120) NOT NULL,
-director_id INT UNSIGNED NOT NULL,
-actors VARCHAR(255),
-plot TEXT,
-rating CHAR(1),
-year VARCHAR(4) NOT NULL,
-genre VARCHAR(120),
-poster VARCHAR(255),
-FOREIGN KEY (director_id) REFERENCES directors(id)
+create table if not exists movies
+(
+    id       int unsigned not null auto_increment primary key,
+    title    varchar(120),
+    rating   char(1),
+    year     char(4),
+    poster   text,
+    plot     text,
+    director_id int unsigned not null,
+    foreign key (director_id) references directors(id)
 );
 
-DESC movies;
+# 6a. Run the script to make sure it works
+describe movies;
+describe directors;
 
+# 7. refactor to extract the directors to a new table with just an id and name
+# --> change the movies table to reference the directors table via Foreign Key
+# --> now that movies is dependent on directors, you need to move directors above movies in the script
+
+# 8. Go add DROP IF EXIST statements for movies and directors
+
+# 9. RUN IT!
 
 create table if not exists genres
 (
-    id   int unsigned not null auto_increment primary key,
-    name varchar(120)
+    id int unsigned not null auto_increment primary key,
+    name varchar(255)
 );
 
-DESC genres;
-
+describe genres;
 
 create table if not exists movie_genre
 (
@@ -63,7 +61,7 @@ create table if not exists movie_genre
     foreign key (genre_id) references genres(id)
 );
 
-DESC movie_genre;
+describe movie_genre;
 
 create table if not exists actors
 (
@@ -71,14 +69,40 @@ create table if not exists actors
     name varchar(255)
 );
 
-DESC actors;
+describe actors;
 
 create table if not exists movie_actor
 (
     movie_id int unsigned not null,
     actor_id int unsigned not null,
-    foreign key (movie_id) references movies (id),
-    foreign key (actor_id) references actors (id)
+    foreign key (movie_id) references movies(id),
+    foreign key (actor_id) references actors(id)
 );
 
-DESC movie_actor;
+describe movie_actor;
+
+INSERT INTO genres (name)
+VALUES ('comedy'),
+       ('drama'),
+       ('action'),
+       ('fantasy'),
+       ('horror'),
+       ('romance'),
+       ('hallmark romance'),
+       ('thiller');
+
+
+INSERT INTO directors (name)
+VALUES ('Director John'),
+       ('Director Susie'),
+       ('Director Tom');
+# create a genres table with two columns: id and name
+
+
+# create a many-to-many relationship b/t movies and genres by creating a movie_genre table.
+# --> It needs to only contain a movie_id and genre_id (be sure to foreign key those to their respective tables)
+
+# create create an actors table with two columns: id and name
+
+# create a many-to-many relationship b/t movies and actors by creating movie_actor table.
+# --> It needs to only contain a movie_id and actor_id (be sure to foreign key those to their respective tables)
